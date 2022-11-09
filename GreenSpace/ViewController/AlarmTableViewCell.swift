7ee94428ced267e18f42bee026c818688296f8a2
it794613 <it794613@naver.com>
2022-11-09 15:56:17 +0900

    AlarmViewController and AlarmTableViewCell

---
+++ GreenSpace/ViewController/AlarmTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  AlarmTableViewCell.swift
+//  GreenSpace
+//
+//  Created by 최진용 on 2022/11/09.
+//
+
+import UIKit
+
+class AlarmTableViewCell: UITableViewCell{
+    
+}
--- GreenSpace/ViewController/AlarmViewController.swift
+++ GreenSpace/ViewController/AlarmViewController.swift
@@ -8,7 +8,36 @@ import UIKit
 
 class AlarmViewController: UIViewController{
     
+    @IBOutlet weak var alarmTableView: UITableView!
+    
     override func viewDidLoad(){
         super.viewDidLoad()
+        self.alarmTableView.delegate = self
+        self.alarmTableView.dataSource = self
     }
+    
+}
+
+//MARK: - 테이블뷰 델리겟
+extension AlarmViewController: UITableViewDelegate{
+    
+}
+
+//MARK: -테이블뷰 데이터 소스
+//TODO: - 데이터 넣기.
+extension AlarmViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = alarmTableView.dequeueReusableCell(withIdentifier: String(String(describing: AlarmTableViewCell))) as! AlarmTableViewCell
+        
+        
+        return cell
+    }
+    
+    
 }
