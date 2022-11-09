1d8e2326c2f7eca251cf7d742fe5484889df48ee
it794613 <it794613@naver.com>
2022-11-09 14:05:11 +0900

    good

--- GreenSpace/ViewController.swift
+++ GreenSpace/ViewController.swift
@@ -11,9 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hellow")
         // Do any additional setup after loading the view.
     }
-
 
 }
 
